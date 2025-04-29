58d9653b6281afb6acea20fa17fe35b56bd35070
Fazyl2023 <khusaenov.fazyl@mail.ru>
2025-04-29 07:19:09 +0300

    add async

--- main.py
+++ main.py
@@ -1,90 +1,89 @@
-
+import aiofiles
 from bs4 import BeautifulSoup as bs
-import requests
 import csv
+import httpx
 from fake_useragent import UserAgent
 import datetime
-from apscheduler.schedulers.background import BackgroundScheduler
-
-
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import asyncio
 class newsBenzinga:
        
-    def __init__(self, url: str):
+    def __init__(self):
         # Время новости
         self.news_time = ''
         # Заголовок новости
         self.title = ''
         # ссылка на новость
         self.news_url = ''
-        self.url = url
+        self.url = 'https://www.Benzinga.com/markets'
 
-    # Установка соединения
-    def soed(self):
-           headers = {
-                    'User-agent': UserAgent().random
-                    }
-           response = requests.get(self.url, headers=headers)
-           print(response.status_code)
-
-           html = bs(response.content, 'html.parser')
-           self.csv_head = ['time', 'title', 'url']
-           return html
-    
     # Считывание всех строк с файла .сsv для исключения повторений новостей
-    def except_url(self):
+    async def except_url(self):
        try:
            all_rows = []
-           with open('./news.csv', 'r') as f: 
-              read = csv.reader(f, delimiter=',')
-              for row in read:
-                  for i in row:
-                      all_rows.append(i)
+           async with aiofiles.open('./newsAsync1.csv', 'r') as f: 
+               async for line in f:
+                    all_rows.extend(line.strip().split(","))            
        except FileNotFoundError:
             pass
        return all_rows
 
     # Функция будет записывать в файл полученные данные
-    def write_file(self):
-        HTML = self.soed()
+    
+    async def write_file(self, str_n):
+        
         # Список одной строчки csv файла (время, заголовок, ссылка)
-        str_n = []
-        # список нужен для того, чтобы новости не повторялись в csv
-           
-        with open('./news.csv', 'a') as f:                    
-                   writer = csv.writer(f)
-                   if self.csv_head not in self.except_url():
-                       writer.writerow(self.csv_head)
-               
-                   for i in range(len(HTML.select('div.post-card-text-wrapper a.post-card-article-link'))):    
+        csv_head = ['time', 'title', 'url']   
+        async with aiofiles.open('./newsAsync1.csv', 'a') as f:                    
+                   
+                  if csv_head[2] not in await self.except_url():
+                       await f.write(f"{','.join(map(str, csv_head))}\n")   
+                        
+                  return await f.write(','.join(map(str, str_n)).replace('\n', '') + '\n')
+     
+    # Парсинг новостей                  
+    async def parsing(self, HTML):
+         
+         str_n = []
+         for i in range(len(HTML.select('div.post-card-text-wrapper a.post-card-article-link'))):    
                            news_time = (datetime.datetime.now().time())
                            title = (HTML.select('div.post-card-text-wrapper a.post-card-article-link')[i].text)
                            news_url = (self.url + HTML.select('div.post-card-text-wrapper a.post-card-article-link')[i]['href'])
                             
-                           if title == '' or news_url == '':
-                                   str_n.append('Null')
-                           # Исключаем повторяющиеся новости       
-                           if news_url not in self.except_url():
+                           # Исключаем повторяющиеся новости и пустые строки      
+                           if news_url not in await self.except_url():
                                str_n.append(news_time)
-                               str_n.append(title)
+                               
+                               str_n.append(str(title).replace(",", ''))
+                               
                                str_n.append(news_url)
-                           # Запись в файл уникальных новостей     
-                           writer.writerow(str_n)
+                               
+                           if str_n != []:
+                                 await self.write_file(str_n)
                            str_n = []
 
 
-def main():
-    
-    url = 'https://www.Benzinga.com/markets'  
-    news_1 = newsBenzinga(url)
 
+async def main():
+    
+    news_1 = newsBenzinga()
+    # Установка соединения
+    headers = {'User-agent': UserAgent().random}
+    async with httpx.AsyncClient() as client:
+        
+        r = await client.get(url = news_1.url, headers = headers)
+        print(r.status_code)
+        html = bs(r.content, 'html.parser')
+    
+    
     # Задаю параметры для автообновления csv таблицы
-    scheduler = BackgroundScheduler()
+    scheduler = AsyncIOScheduler()
     time = int(input("Введите через сколько секунд обнолвять таблицу: "))
-    scheduler.add_job(news_1.write_file, 'interval', seconds = time)
+    
+    scheduler.add_job(news_1.parsing, 'interval', seconds = time, args=[html])
     scheduler.start()
+    await asyncio.Event().wait()
 
 if __name__ == "__main__":
-     while True:
-         main()
-
+         asyncio.run(main())
 
